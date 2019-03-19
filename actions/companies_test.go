9ae202a49b8eb405b85f7eb6fc37f01c8f26caa6
Jayesh Mistry <jayesh.mistry@optimalworkshop.com>
2019-03-19 17:12:46 +1300

    Add basic company model and actions

---
+++ actions/companies_test.go
@@ -0,0 +1,35 @@
+package actions
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func (as *ActionSuite) Test_CompaniesResource_List() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_CompaniesResource_Show() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_CompaniesResource_Create() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_CompaniesResource_Update() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_CompaniesResource_Destroy() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_CompaniesResource_New() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_CompaniesResource_Edit() {
+	as.Fail("Not Implemented!")
+}
---
+++ models/company.go
@@ -0,0 +1,48 @@
+package models
+
+import (
+	"encoding/json"
+	"github.com/gobuffalo/pop"
+	"github.com/gobuffalo/validate"
+	"github.com/gofrs/uuid"
+	"time"
+)
+
+type Company struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+// String is not required by pop and may be deleted
+func (c Company) String() string {
+	jc, _ := json.Marshal(c)
+	return string(jc)
+}
+
+// Companies is not required by pop and may be deleted
+type Companies []Company
+
+// String is not required by pop and may be deleted
+func (c Companies) String() string {
+	jc, _ := json.Marshal(c)
+	return string(jc)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (c *Company) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (c *Company) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (c *Company) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
