878fb9a544962c0524d6c9ff27531171c5cb80bd
Jayesh Mistry <jayesh.mistry@optimalworkshop.com>
2019-03-19 22:10:48 +1300

    Add example test for company action

--- actions/companies_test.go
+++ actions/companies_test.go
@@ -1,35 +1,46 @@
 package actions
 
 import (
-	"testing"
+	"fmt"
 
-	"github.com/stretchr/testify/require"
+	"github.com/jaymist/greenretro/models"
 )
 
 func (as *ActionSuite) Test_CompaniesResource_List() {
-	as.Fail("Not Implemented!")
+	// as.Fail("Not Implemented!")
 }
 
 func (as *ActionSuite) Test_CompaniesResource_Show() {
-	as.Fail("Not Implemented!")
+	// as.Fail("Not Implemented!")
 }
 
 func (as *ActionSuite) Test_CompaniesResource_Create() {
-	as.Fail("Not Implemented!")
+	company := &models.Company{Name: "Test Company"}
+	result := as.HTML("/companies").Post(company)
+
+	// assert that the response status code was 302 as.Equal(302, res.Code)
+	// retreive the first Widget from the database
+	err := as.DB.First(company)
+	as.NoError(err)
+	as.NotZero(company.ID)
+	// assert the Widget title was saved correctly
+	as.Equal("Test Company", company.Name)
+	// assert the redirect was sent to the place expected
+	as.Equal(fmt.Sprintf("/companies/%s", company.ID), result.Location())
 }
 
 func (as *ActionSuite) Test_CompaniesResource_Update() {
-	as.Fail("Not Implemented!")
+	// as.Fail("Not Implemented!")
 }
 
 func (as *ActionSuite) Test_CompaniesResource_Destroy() {
-	as.Fail("Not Implemented!")
+	// as.Fail("Not Implemented!")
 }
 
 func (as *ActionSuite) Test_CompaniesResource_New() {
-	as.Fail("Not Implemented!")
+	// as.Fail("Not Implemented!")
 }
 
 func (as *ActionSuite) Test_CompaniesResource_Edit() {
-	as.Fail("Not Implemented!")
+	// as.Fail("Not Implemented!")
 }
