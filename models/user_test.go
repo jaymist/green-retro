600bce43174a9e8276820c932069903103840b86
Jayesh Mistry <jayesh.ish.mistry@gmail.com>
2019-03-26 21:07:41 +1300

    Add user model validation tests
    Tests currently fail.

--- models/user_test.go
+++ models/user_test.go
@@ -11,6 +11,8 @@ func (ms *ModelSuite) Test_User_Create() {
 
 	u := &models.User{
 		Email:                "mark@example.com",
+		FirstName:            "Mark",
+		LastName:             "Example",
 		Password:             "password",
 		PasswordConfirmation: "password",
 	}
@@ -24,6 +26,75 @@ func (ms *ModelSuite) Test_User_Create() {
 	count, err = ms.DB.Count("users")
 	ms.NoError(err)
 	ms.Equal(1, count)
+}
+
+func (ms *ModelSuite) TestUserCreateWithMissingEmail() {
+	count, err := ms.DB.Count("users")
+	ms.NoError(err)
+	ms.Equal(0, count)
+
+	u := &models.User{
+		Password:             "password",
+		PasswordConfirmation: "password",
+	}
+	ms.Zero(u.PasswordHash)
+
+	verrs, err := u.Create(ms.DB)
+	ms.NoError(err)
+	ms.True(verrs.HasAny())
+	ms.Equal(1, len(verrs.Keys()))
+	ms.Equal("email", verrs.Keys()[0])
+	ms.EqualError(verrs, "Email can not be blank.")
+
+	count, err = ms.DB.Count("users")
+	ms.NoError(err)
+	ms.Equal(0, count)
+}
+
+func (ms *ModelSuite) TestUserCreateWithMissingPassword() {
+	count, err := ms.DB.Count("users")
+	ms.NoError(err)
+	ms.Equal(0, count)
+
+	u := &models.User{
+		Email: "mark@example.com",
+	}
+	ms.Zero(u.PasswordHash)
+
+	verrs, err := u.Create(ms.DB)
+	ms.NoError(err)
+	ms.True(verrs.HasAny())
+	ms.Equal(1, len(verrs.Keys()))
+	ms.Equal("password", verrs.Keys()[0])
+	ms.EqualError(verrs, "Password can not be blank.")
+
+	count, err = ms.DB.Count("users")
+	ms.NoError(err)
+	ms.Equal(0, count)
+}
+
+func (ms *ModelSuite) TestUserCreateWithMissingFirstName() {
+	count, err := ms.DB.Count("users")
+	ms.NoError(err)
+	ms.Equal(0, count)
+
+	u := &models.User{
+		Email:                "mark@example.com",
+		Password:             "password",
+		PasswordConfirmation: "password",
+	}
+	ms.Zero(u.PasswordHash)
+
+	verrs, err := u.Create(ms.DB)
+	ms.NoError(err)
+	ms.True(verrs.HasAny())
+	ms.Equal(1, len(verrs.Keys()))
+	ms.Equal("name", verrs.Keys()[0])
+	ms.EqualError(verrs, "Name can not be blank.")
+
+	count, err = ms.DB.Count("users")
+	ms.NoError(err)
+	ms.Equal(0, count)
 }
 
 func (ms *ModelSuite) Test_User_Create_ValidationErrors() {
