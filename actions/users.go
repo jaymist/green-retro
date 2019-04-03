c705a26c5f5c60dc0dfdfebd1b57d971492ff4fc
Jayesh Mistry <jayesh.ish.mistry@gmail.com>
2019-04-03 20:47:05 +1300

    Extract 'authorization' header from request
    Decode header value ready to pass to authentication function.

--- actions/users.go
+++ actions/users.go
@@ -1,6 +1,10 @@
 package actions
 
 import (
+	"encoding/base64"
+	"net/http"
+	"strings"
+
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/pop"
 	"github.com/jaymist/greenretro/models"
@@ -77,7 +81,34 @@ func Authorize(next buffalo.Handler) buffalo.Handler {
 // BasicAuthorize adds basic auth support to requests.
 func BasicAuthorize(next buffalo.Handler) buffalo.Handler {
 	return func(c buffalo.Context) error {
-		c.Logger().Info("Running basic auth check...\n")
-		return nil
+		authHeader := c.Request().Header.Get("Authorization")
+		if authHeader == "" {
+			c.Response().Header().Set("WWW-Authenticate", `Basic realm="Basic Authentication"`)
+			return c.Error(http.StatusUnauthorized, errors.New("Unauthorized"))
+		}
+		c.Logger().WithField("authorization", authHeader).Info("Header")
+
+		fields := strings.Split(authHeader, " ")
+		if len(fields) < 2 {
+			c.Response().Header().Set("WWW-Authenticate", `Basic realm="Basic Authentication"`)
+			return c.Error(http.StatusUnauthorized, errors.New("Unauthorized"))
+		}
+		token := fields[1]
+		c.Logger().WithField("token", token).Info("Token")
+
+		b, err := base64.StdEncoding.DecodeString(token)
+		if err != nil {
+			c.Response().Header().Set("WWW-Authenticate", `Basic realm="Basic Authentication"`)
+			return c.Error(http.StatusUnauthorized, errors.New("Unauthorized"))
+		}
+
+		pair := strings.SplitN(string(b), ":", 2)
+		params := map[string]string{
+			"user":     pair[0],
+			"password": pair[1],
+		}
+
+		c.Logger().WithField("user details", params).Info("User details")
+		return next(c)
 	}
 }
