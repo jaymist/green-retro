02d18afb78e76ea5df26a732466f3ece117896c8
Jayesh Mistry <jayesh.ish.mistry@gmail.com>
2019-04-08 08:30:12 +1200

    Add test for invalid input
    Send incomplete user model to the service and confirm that the users/teams/users_teams records aren't created.

--- services/user_team_test.go
+++ services/user_team_test.go
@@ -37,3 +37,39 @@ func (ss *ServiceSuite) Test_User_Team_Create() {
 	ss.NoError(err)
 	ss.Equal(1, count)
 }
+
+func (ss *ServiceSuite) Test_User_Team_Create_With_Empty_Name() {
+	count, err := ss.DB.Count("teams")
+	ss.NoError(err)
+	ss.Equal(0, count)
+
+	count, err = ss.DB.Count("users")
+	ss.NoError(err)
+	ss.Equal(0, count)
+
+	u := &models.User{
+		LastName: "Bunny",
+		Email:    "bugs@acme.com",
+		Password: "password",
+	}
+	ss.Zero(u.PasswordHash)
+
+	verrs, err := services.CreateUserAndTeam(ss.DB, u)
+	ss.NoError(err)
+	ss.True(verrs.HasAny())
+	ss.Equal(1, len(verrs.Keys()))
+	ss.Equal("first_name", verrs.Keys()[0])
+	ss.EqualError(verrs, "Name can not be blank.")
+
+	count, err = ss.DB.Count("teams")
+	ss.NoError(err)
+	ss.Equal(0, count)
+
+	count, err = ss.DB.Count("users")
+	ss.NoError(err)
+	ss.Equal(0, count)
+
+	count, err = ss.DB.Count("users_teams")
+	ss.NoError(err)
+	ss.Equal(0, count)
+}
