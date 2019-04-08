4de447fa1a831424109b79b0696fa702321439fc
Jayesh Mistry <jayesh.ish.mistry@gmail.com>
2019-04-08 17:14:06 +1200

    Add test to confirm user registration form also creates corresponding team
    Increase test coverage for action.

--- actions/users_test.go
+++ actions/users_test.go
@@ -1,6 +1,8 @@
 package actions_test
 
 import (
+	"net/http"
+
 	"github.com/jaymist/greenretro/models"
 )
 
@@ -27,4 +29,33 @@ func (as *ActionSuite) Test_Users_Create() {
 	count, err = as.DB.Count("users")
 	as.NoError(err)
 	as.Equal(1, count)
+
+	count, err = as.DB.Count("teams")
+	as.NoError(err)
+	as.Equal(1, count)
+
+	err = as.DB.Eager().Last(u)
+	as.NoError(err)
+
+	as.NotEmpty(u.Teams)
+	as.Equal(u.FirstName, u.Teams[0].Name)
+}
+
+func (as *ActionSuite) Test_Users_Create_Without_Email() {
+	count, err := as.DB.Count("users")
+	as.NoError(err)
+	as.Equal(0, count)
+
+	u := &models.User{
+		FirstName: "Mark",
+		LastName:  "Example",
+		Password:  "password",
+	}
+
+	res := as.HTML("/users").Post(u)
+	as.Equal(http.StatusBadRequest, res.Code)
+
+	count, err = as.DB.Count("users")
+	as.NoError(err)
+	as.Equal(0, count)
 }
